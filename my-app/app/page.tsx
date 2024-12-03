dfafb1b35ae59eb30607eacb4a72b37944ac0ebc
Husnain Izhar <husnainizhar@outlook.com>
2024-12-03 14:18:28 +0000

    changes made to the file

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Chandged</li>
+          <li>Changes Made to the file</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
