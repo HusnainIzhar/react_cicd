b30d6f5c4d5b1cb3447fe0c44330075d46456a41
Husnain Izhar <husnainizhar@outlook.com>
2024-12-04 17:26:38 +0000

    latest

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="flex bg-yellow-500">
-      <h1>its Working</h1>
+    <div className="flex bg-yellow-500 h-screen">
+      <h1 className="text-2xl py-20">its Working</h1>
     </div>
   );
 }
