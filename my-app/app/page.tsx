49a93e941edc706ce27325592400f0e40bde615a
Husnain Izhar <husnainizhar@outlook.com>
2024-12-07 01:08:23 +0500

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
       <h1 className="text-4xl py-20">Changefjfdsffdfdssfdksjs CI BRANCHING</h1>
 
-      <h1 className="text-4xl py-20">lets fffdsfdfdssdsjkljcfdsfdshafdsnge</h1>
+      <h1 className="text-4xl py-20">lets fffdsffdsdfdssdsjkljcfdsfdshafdsnge</h1>
     </div>
   );
 }
