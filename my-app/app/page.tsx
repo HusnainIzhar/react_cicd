67dbe7b62ba10cee06e02030a93f984dbf3f2a90
Husnain Izhar <husnainizhar@outlook.com>
2024-12-07 00:22:28 +0500

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex bg-black h-screen items-center justify-center">
 
-      <h1 className="text-4xl py-20">Changefjdksjs CI BRANCHING</h1>
+      <h1 className="text-4xl py-20">Changefjfdsfdksjs CI BRANCHING</h1>
 
       <h1 className="text-4xl py-20">lets change</h1>
     </div>
