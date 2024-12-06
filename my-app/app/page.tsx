83faf2a9028a8c1239c6afeaf14feff05d617141
Husnain Izhar <husnainizhar@outlook.com>
2024-12-07 00:16:24 +0500

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex bg-black h-screen items-center justify-center">
-      <h1 className="text-4xl py-20">Changefjdksjs CI BRANCHINGfdf</h1>
+      <h1 className="text-4xl py-20">Changefjdksjs CI BRANCHIgfdgNGfdf</h1>
     </div>
   );
 }
